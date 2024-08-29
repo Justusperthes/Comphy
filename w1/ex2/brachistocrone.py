1c363bb9ce7a7e97ff98914c04d2caf905b9ed25
Justusperthes <jacob.hoey@gmail.com>
2024-08-29 12:02:55 +0200

    w1ex2: brach drawn

--- w1/ex2/brachistocrone.py
+++ w1/ex2/brachistocrone.py
@@ -5,25 +5,43 @@ import matplotlib.animation as animation
 from matplotlib.animation import PillowWriter
 from scipy.optimize import fsolve
 
+eps = 1e-8
 
+# solve for phi1
 def func1(phi1):
-    eps = 1e-8
     return (np.cos(phi1)-1)/(phi1-np.sin(phi1)+eps)+3/4
-root = fsolve(func1,10)[0]
-print(root)
+phi1 = fsolve(func1,10)[0]
 
+# brachistochrone must pass through (30,0)
 x = 30
 y = 0
-
+# solve for a, y0
 def func2(z):
-    eq1 = z[0] * (root - np.sin(root)) - x
-    eq2 = z[0] * (1 + np.cos(root)) + z[1] - y
+    eq1 = z[0] * (phi1 - np.sin(phi1)) - x
+    eq2 = z[0] * (1 + np.cos(phi1)) + z[1] - y
     return [eq1, eq2]
-root = fsolve(func2, [100, 10])
-print(root)
+a_y0 = fsolve(func2, [100, 10])
+a = a_y0[0]
+y0 = a_y0[1]
 
-# def func(x):
-#     return [x[0] * np.cos(x[1]) - 4,
-#             x[1] * x[0] - x[1] - 5]
-# root = fsolve(func, [1, 1])
-# print(root)
+# parametric equation
+def x(phi):
+    return a * (phi - np.sin(phi))
+def y(phi):
+    return a * (1 + np.cos(phi)) + y0
+
+# generate range of phi values
+phi = np.linspace(0, phi1, 1000)  # Adjust the range as needed
+
+x_values = x(phi)
+y_values = y(phi)
+
+# plot parametric curve
+plt.plot(x_values, y_values, label=r'$x(\phi), y(\phi)$')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Parametric Plot of x = x(φ), y = y(φ)')
+plt.legend()
+plt.grid(True)
+plt.savefig('brachistochrone.png')
+plt.close()
---
+++ w1/ex3/triangles.py
@@ -0,0 +1,9 @@
+class Triangles:
+    # constructor
+    def __init__(self, points, cm, color, triangle, cross):
+        self.points = #list of 3 tuples
+        self.cm = #tuple
+        self.color = #string color
+        ...
+    
+    #methods
